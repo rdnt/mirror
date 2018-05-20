c09c614221bb56b9a1daf4089da9632920a73092
ShtHappens796 <shithappens796@gmail.com>
2018-05-20 18:38:08 +0300

    Temperature

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -1,11 +1,18 @@
 <div class="mirror">
-    <div class="segment">
-        
+    <div class="segment one">
+        <div class="temp">
+            <span>27°</span>
+        </div>
+        <div class="weather-icon">
+        </div>
     </div>
-    <div class="segment">
+    <div class="segment two">
+        test
     </div>
-    <div class="segment">
+    <div class="segment three">
+        test
     </div>
-    <div class="segment">
+    <div class="segment four">
+        test
     </div>
 </div>

0204a61f9c13a75954a8d5a32fa56caeebf73c9e
ShtHappens796 <shithappens796@gmail.com>
2018-05-20 22:18:05 +0300

    Fix header description

--- includes/components/head.php
+++ includes/components/head.php
@@ -2,6 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <link href="/css/mirror.css?v=<?=$this->getLatestCommit()?>" type="text/css" rel="stylesheet" media="screen"/>
 
-<meta name="description" content="A web interface built to be displayed on a monitor behind a two-way mirror">
+<meta name="description" content="A web interface built to be displayed on a monitor behind a two-way mirror.">
 <meta name="keywords" content="Web Mirror,Mirror,Open-Source,Github,ShtHappens796,SHT">
 <meta name="author" content="ShtHappens796">

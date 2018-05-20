70732bbc48932af838793a5f8ea52c589d5ce1a2
ShtHappens796 <shithappens796@gmail.com>
2018-05-20 19:39:34 +0300

    Add time updating

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -2,32 +2,32 @@
 <div id="mirror">
     <div class="segment one">
         <div class="temperature">
-            <span>27°</span>
+            <span id="temperature">27°</span>
         </div>
         <div class="weather-icon">
         </div>
         <div class="weather-status">
-            <span>Chance of a thunderstorm tonight and partly cloudy tomorrow morning</span>
+            <span id="weather-status">Clear</span>
         </div>
         <div class="precipitation-icon">
 
         </div>
         <div class="precipitation">
-            <span>30%</span>
+            <span id="precipitation">30%</span>
         </div>
     </div>
     <div class="segment two">
         <div class="time">
-            <span>18:02</span>
+            <span id="time"></span>
         </div>
         <div class="date">
-            <span>Sunday<br>May 20</span>
+            <span id="date"></span>
         </div>
         <div class="location-icon">
 
         </div>
         <div class="location">
-            <span>Corfu, Greece</span>
+            <span id="location">Corfu, Greece</span>
         </div>
     </div>
     <div class="segment three">
--- js/mirror.js
+++ js/mirror.js
@@ -1,7 +1,44 @@
 var slider = document.getElementById("slider");
-var mirror = document.getElementById("mirror").style;
+var mirror = document.getElementById("mirror");
 
-// Update the current slider value (each time you drag the slider handle)
+var time = document.getElementById("time");
+var date = document.getElementById("date");
+
+
 slider.oninput = function() {
-    mirror.setProperty("font-size", (this.value/100) + "em");
+    mirror.style.setProperty("font-size", (this.value/100) + "em");
 }
+
+var day = new Array(7);
+day[0] =  "Sunday";
+day[1] = "Monday";
+day[2] = "Tuesday";
+day[3] = "Wednesday";
+day[4] = "Thursday";
+day[5] = "Friday";
+day[6] = "Saturday";
+
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+function update() {
+    var d = new Date();
+    time.innerHTML = d.getHours() + ":" + d.getMinutes();
+    date.innerHTML = day[d.getDay()] + "<br>" + month[d.getMonth()] + " " + d.getDate();
+}
+
+update();
+window.setInterval(function(){
+    update();
+}, 1000);

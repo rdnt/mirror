c34198f6edba570816054444ae80577fa3bc9bda
ShtHappens796 <shithappens796@gmail.com>
2018-05-20 20:36:43 +0300

    Add some checks

--- backend/shell/modules/Weather.php
+++ backend/shell/modules/Weather.php
@@ -2,16 +2,25 @@
 // Trait that handles weather updating and viewing
 trait Weather {
     // Updates the weather from the wUnderground API
-    function updateWeather($api_key, $location) {
-        // Make the necessary requests
-        $api = "https://api.wunderground.com/api/$api_key";
-        $location = urlencode(trim($location));
-        $conditions = file_get_contents("$api/conditions/q/$location.json");
-        $forecast = file_get_contents("$api/forecast/q/$location.json");
-        // Save the data
-        file_put_contents($this->getRoot() . "/data/weather/conditions.json", $conditions);
-        file_put_contents($this->getRoot() . "/data/weather/forecast.json", $forecast);
-        $this->response("SUCCESS");
+    function updateWeather() {
+        $data = $this->getRoot() . "/data/";
+        if (file_exists($data . "key") and file_exists($data . "location")) {
+            // Prepare the query
+            $api_key = trim(file_get_contents($data . "key"));
+            $location = urlencode(trim(file_get_contents($data . "location")));
+            // Set the API backend to send the requests to
+            $api = "https://api.wunderground.com/api/$api_key";
+            // Make the necessary requests
+            $conditions = file_get_contents("$api/conditions/q/$location.json");
+            $forecast = file_get_contents("$api/forecast/q/$location.json");
+            // Save the data
+            file_put_contents($data . "weather/conditions.json", $conditions);
+            file_put_contents($data . "weather/forecast.json", $forecast);
+            $this->response("SUCCESS");
+        }
+        else {
+            $this->response("UPDATE_ERROR");
+        }
     }
 
     function getWeatherConditions() {
--- backend/update.php
+++ backend/update.php
@@ -1,8 +1,5 @@
 <?php
 // Require the SHT Core
 require_once $_SERVER['DOCUMENT_ROOT']."/backend/core/Core.php";
-// Get the API key
-$key = trim(file_get_contents($mirror->getRoot() . "/data/api.key"));
-$location = trim(file_get_contents($mirror->getRoot() . "/data/location"));
 // Make the necessary requests to update the weather data
-//$mirror->updateWeather($key, $location);
+//$mirror->updateWeather();
